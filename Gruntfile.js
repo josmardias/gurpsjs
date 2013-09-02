b42457db6b7eff39e9a92e150e6913787193f9de
Josmar Dias <josmarnet@gmail.com>
2013-09-02 16:26:10 -0300

    Adding jslint to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,32 @@ module.exports = function (grunt) {
         watch: {
             files: ['tests/*.js', 'tests/*.html', 'src/*.js'],
             tasks: ['qunit']
+        },
+        jslint: {
+            source: {
+                src: ['src/*.js'],
+                directives: {
+                    todo: true,
+                    continue: true,
+                    forin: true,
+                    nomen: true,
+                    predef: [
+                        'GURPS'
+                    ]
+                },
+                options: {
+                    errorsOnly: true,
+                    failOnError: false
+                }
+            }
         }
     });
     // load up your plugins
+    grunt.loadNpmTasks('grunt-jslint');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-watch');
     // register one or more task lists (you should ALWAYS have a "default" task list)
-    grunt.registerTask('default', ['qunit']);
+    grunt.registerTask('default', ['jslint', 'qunit']);
     grunt.registerTask('test', ['qunit']);
+    grunt.registerTask('lint', ['jslint']);
 };
