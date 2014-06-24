1337bf88f0b8630df07e12a07f0cd0200d2ebb10
Josmar Dias <josmarnet@gmail.com>
2014-06-23 21:26:25 -0300

    adding exit code on build error

--- travis.sh
+++ travis.sh
@@ -3,11 +3,12 @@
 if [ "$1" == "browserstack" ]; then
     if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         grunt browserstack;
+        exit $?;
     fi
 fi
 
 if [ "$1" == "coverage" ]; then
-    grunt coverage;
+    grunt coverage || exit $?;
 
     if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         codeclimate < coverage/*/lcov.info; 
