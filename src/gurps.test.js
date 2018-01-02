c2142c846ba42efe59df46adb898794ae9f8dba6
Josmar Dias <josmarnet@gmail.com>
2018-01-02 14:02:52 -0200

    first draft of how the API could looks like

---
+++ src/gurps.test.js
@@ -0,0 +1,45 @@
+test.skip('smoke: battle between two characters', () => {
+  const battle = createBattle({
+    john: createCharacter(),
+    peter: createCharacter(),
+  })
+
+  expect(battle).toBeDefined()
+
+  // attack roll - success
+  const johnAttackedPeterAction = createAction(
+    battle,
+    {
+      entity: 'john',
+      skill: 'knife',
+      target: 'peter',
+      diceRoll: [3, 3, 3],
+    }
+  )
+
+  expect(johnAttackedPeterAction).toBeDefined()
+  expect(johnAttackedPeterAction.status).toEqual(STATUS_SUCCESS)
+
+  // defense roll - failure
+  const peterDefenseAgainstJohnAction = createAction(
+    battle,
+    {
+      entity: 'peter',
+      skill: 'parrying',
+      target: johnAttackedPeterAction,
+      diceRoll: [5, 5, 5],
+    }
+  )
+
+  expect(peterDefenseAgainstJohnAction).toBeDefined()
+  expect(peterDefenseAgainstJohnAction.status).toEqual(STATUS_FAILURE)
+
+  // consolidate action in battle
+  const newBattleState = runAction(
+    battle,
+    peterDefenseAgainstJohnAction,
+  )
+
+  expect(newBattleState).toBeDefined()
+  expect(getEntityAttribute('peter', 'HP')).toEqual('????')
+})
